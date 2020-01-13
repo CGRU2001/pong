64f14fce2e9f6cdfd85506d92a53585f2e80e1ca
Carlos <carlosguillermoru@outlook.com>
2020-01-13 22:31:50 +0100

    Implementa método mover

--- classes/Pelota.java
+++ classes/Pelota.java
@@ -12,9 +12,10 @@ import java.awt.geom.Rectangle2D;
  * @author carlos
  */
 public class Pelota {
-    public static final int ANCHO=20,ALTO=20;
+    public static final int ANCHO=20, ALTO = 20;
     private int posicionx;
     private int posiciony;
+    private int dx=1, dy=1;
     public Pelota(){
         //Constructor por defecto
     }
@@ -24,6 +25,22 @@ public class Pelota {
         this.posiciony = posiciony;
     }
     public Rectangle2D getPelota(){
-        return new Rectangle2D.Double(posicionx, posiciony, this.ANCHO, this.ALTO );
+        return new Rectangle2D.Double(posicionx, posiciony, this.ANCHO, this.ALTO ); //Devuelve la posicion de la pelota
+    }
+    public void mover(Rectangle2D limites){
+        this.posicionx+=this.dx;
+        this.posiciony+=this.dy;
+        if(posicionx>limites.getMaxX()){
+            this.dx=-this.dx;
+        }
+        if(posiciony>limites.getMaxY()){
+            this.dy=-this.dy;
+        }
+        if(posicionx<0){
+            this.dx=-this.dx;
+        }
+        if(posiciony<0){
+            this.dy=-this.dy;
+        }
     }
 }

edcd86ff4697587e088ba06e0255fd100cdc7e36
Carlos <carlosguillermoru@outlook.com>
2020-01-13 22:33:05 +0100

    Creamos método actualizar

--- classes/Table.java
+++ classes/Table.java
@@ -30,5 +30,7 @@ public class Table extends JPanel{
     public void dibujar(Graphics2D g){
         g.fill(pelota.getPelota());
     }
-    
+    public void actualizar(){
+        this.pelota.mover(getBounds());
+    }
 }

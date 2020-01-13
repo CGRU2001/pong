9da2f30b400c3e088c9c328816fadd9c7c9769f0
Carlos <carlosguillermoru@outlook.com>
2020-01-13 22:57:06 +0100

    Creamos el método getRaqueta

--- classes/Raqueta.java
+++ classes/Raqueta.java
@@ -5,15 +5,23 @@
  */
 package classes;
 
+import java.awt.geom.Rectangle2D;
+
 /**
  *
  * @author carlos
  */
 public class Raqueta {
-    private int x;
-    private int y;
+    
+    public static final int ANCHO=10, ALTO=25;
+    
+    private int x, y;
+    
     public Raqueta(int x, int y){
         this.x=x;
         this.y=y;
+    }
+    public Rectangle2D getRaqueta(){
+        return new Rectangle2D.Double(x, y, ANCHO, ALTO);
     }
 }

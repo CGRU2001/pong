60a1fd48c28f1651761d9d19ae99f92f1c0bf6d0
Carlos <carlosguillermoru@outlook.com>
2020-01-13 22:55:33 +0100

    Creamos la clase raquetas

---
+++ classes/Raqueta.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author carlos
+ */
+public class Raqueta {
+    private int x;
+    private int y;
+    public Raqueta(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+}

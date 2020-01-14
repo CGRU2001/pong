ea99222ca443c66080d30b3e2b96798b78b7fdd8
Carlos <carlosguillermoru@outlook.com>
2020-01-14 15:46:54 +0100

    Sobreescribe las clases KeyPressed

--- classes/Events.java
+++ classes/Events.java
@@ -6,6 +6,7 @@
 package classes;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 /**
  *
@@ -13,6 +14,44 @@ import java.awt.event.KeyAdapter;
  */
 public class Events extends KeyAdapter{
     public static boolean w, s, up, down;
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int id = e.getKeyCode();
+        
+        if (id == KeyEvent.VK_W) {
+            w = true;
+        }
+        if (id == KeyEvent.VK_S) {
+            s = true;
+        }
+        if (id == KeyEvent.VK_UP) {
+            up = true;
+        }
+        if (id == KeyEvent.VK_DOWN) {
+            down = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+        int id = e.getKeyCode();
+        
+        if (id == KeyEvent.VK_W) {
+            w = false;
+        }
+        if (id == KeyEvent.VK_S) {
+            s = false;
+        }
+        if (id == KeyEvent.VK_UP) {
+            up = false;
+        }
+        if (id == KeyEvent.VK_DOWN) {
+            down = false;
+        }
+    }
     
-    
+
 }
